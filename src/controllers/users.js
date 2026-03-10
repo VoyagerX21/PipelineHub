87d4231f6f953f921e0ab2d015eb7802706244cf
VoyagerX21 <khakse2gaurav2003@gmail.com>
2026-03-10 15:36:59 +0000

    Deleted the slack webhook if the user wanted to clear it

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,12 +2,12 @@ const User = require("../models/User");
 const Webhook = require('../models/Webhook');
 
 const updateUserConfig = async (req, res) => {
-    try{
+    try {
         const userId = req.params.userId;
         const user = await User.findById(userId);
         const { slackChannel, slackToken } = req.body;
-        
-        if (!user){
+
+        if (!user) {
             return res.status(401).json({
                 msg: "User not found"
             });
@@ -16,21 +16,26 @@ const updateUserConfig = async (req, res) => {
         user.SlackChannelName = slackChannel;
         user.SlackURL = slackToken;
         await user.save();
-        const webhook = await Webhook.findOne({userId: userId});
-        if (!webhook){
+        const webhook = await Webhook.findOne({ userId: userId });
+        if (!webhook) {
             await Webhook.create({
                 userId: userId,
                 name: slackChannel,
                 targetUrl: slackToken,
             });
         }
-        else{
-            await Webhook.findOneAndUpdate({userId: userId}, {
-                $set: {
-                    name: slackChannel,
-                    targetUrl: slackToken
-                }
-            });
+        else {
+            if (slackToken) {
+                await Webhook.findOneAndUpdate({ userId: userId }, {
+                    $set: {
+                        name: slackChannel,
+                        targetUrl: slackToken
+                    }
+                });
+            }
+            else{
+                await Webhook.deleteOne({ userId: userId });
+            }
         }
         return res.status(200).json({ success: true, message: "Configuration saved successfully" });
     } catch (err) {
